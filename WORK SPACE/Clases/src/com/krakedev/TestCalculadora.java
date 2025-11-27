dd251a45a5a7867b7a7a35fd9f69602ba1291560
Coug4r <burneofrancisco2005@gmail.com>
2025-11-27 12:42:10 -0500

    Se invocan todas las funciones creadas en TestCalculadora

--- WORK SPACE/Clases/src/com/krakedev/TestCalculadora.java
+++ WORK SPACE/Clases/src/com/krakedev/TestCalculadora.java
@@ -8,7 +8,15 @@ public class TestCalculadora {
 		calcu = new Calculadora();
 		int resultado = calcu.sumar(5, 8);
 		int resultadoResta = calcu.restar(20, 10);
+		double resultadoMult = calcu.multiplicar(2, 10);
+		double resultadoDiv = calcu.dividir(2, 10);
+		double resultadoProm = calcu.promediar(2, 10, 5);
+		String msg = calcu.mostrarResultado();
 		System.out.println("RESULTADO SUMA: "+resultado);
 		System.out.println("RESULTADO RESTA: "+ resultadoResta);
+		System.out.println("RESULTADO MULTIPLICACION: "+ resultadoMult);
+		System.out.println("RESULTADO DIVISION: "+ resultadoDiv);
+		System.out.println("RESULTADO PROMEDIO: "+ resultadoProm);
+		System.out.println(msg);
 	}
 }
