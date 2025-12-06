ec8e91509d79889f717e976ebf881a69d0896f96
Coug4r <burneofrancisco2005@gmail.com>
2025-12-06 11:35:45 -0500

    Ejercicio 2 se crea la clase Estudiantes y se prueba el metodo calificar

---
+++ WORK SPACE/Repaso/src/com/cmc/repaso/test/TestEstudiante.java
@@ -0,0 +1,10 @@
+package com.cmc.repaso.test;
+import com.cmc.repaso.entidades.Estudiante;
+public class TestEstudiante {
+
+	public static void main(String[] args) {
+		Estudiante est1 = new Estudiante("David");
+		est1.calificar(8);
+	}
+
+}
